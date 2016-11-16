7d9638f92c9ab1450206c44e15e75d0f7484f9fb
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2016-11-16 01:11:19 -0800

    .

--- sites/all/themes/fire/js/custom.js
+++ sites/all/themes/fire/js/custom.js
@@ -1,13 +1,13 @@
-// (function($) {
-//   // This jQuery function is called when the document is ready
-//   $(function() {
-//      $('form.comment-form').hide();
-//      $('#comments h2.comment-form').click(function() {
-//         $('form.comment-form').toggle('slow', function() {
-//           // Animation complete.
-//         });
-//         //add css class to H2 title when clicked//
-//         $(this).toggleClass("open");
-//       });
-//   });
-// })(jQuery);
+(function($) {
+  // This jQuery function is called when the document is ready
+  $(function() {
+     $('form.comment-form').hide();
+     $('#comments h2.comment-form').click(function() {
+        $('form.comment-form').toggle('slow', function() {
+          // Animation complete.
+        });
+        //add css class to H2 title when clicked//
+        $(this).toggleClass("open");
+      });
+  });
+})(jQuery);
