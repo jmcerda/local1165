f414530f621cf655e4a0f42cfd5595632c7eed0f
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2016-12-21 02:41:09 -0800

    aside work

--- sites/all/themes/fire/templates/page.tpl.php
+++ sites/all/themes/fire/templates/page.tpl.php
@@ -139,7 +139,7 @@ global $user;
       </div>
     </div>
     <?php if ($page['right_sidebar']) : ?>
-      <aside id="right-sidebar" class="col-xs-12 col-sm-2">
+      <aside id="right-sidebar" class="visible-sm-block col-xs-12 col-sm-2">
         <?php print render($page['right_sidebar']); ?>
       </aside>
     <?php endif; ?>
